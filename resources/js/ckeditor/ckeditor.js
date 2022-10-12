08850abdd9d8eb37a40000f3b391cb948f98db5c
Karel Faille <shaffe.fr@gmail.com>
2022-10-12 14:29:57 +0200

    add remove format plugin

--- resources/js/ckeditor/ckeditor.js
+++ resources/js/ckeditor/ckeditor.js
@@ -22,6 +22,7 @@ import Font from '@ckeditor/ckeditor5-font/src/font'
 
 // Styles & Enhancements
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice'
+import RemoveFromat from '@ckeditor/ckeditor5-remove-format/src/removeformat'
 import StrikeThrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough'
 import Code from '@ckeditor/ckeditor5-basic-styles/src/code'
 import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript'
@@ -103,6 +104,7 @@ export default class CkEditor extends ClassicEditorBase {
             Superscript,
             StrikeThrough,
             Code,
+            RemoveFromat,
             Image,
             ImageStyle,
             ImageUpload,
