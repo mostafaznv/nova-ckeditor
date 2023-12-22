84301c5c7a7ab5fffb495beb6eea724c061cb1d7
mostafaznv <mostafa.zeinivand@gmail.com>
2023-12-22 22:42:59 +0330

    feat: support intervention/image:^3.0 #103

--- src/ImageStorage.php
+++ src/ImageStorage.php
@@ -3,10 +3,10 @@
 namespace Mostafaznv\NovaCkEditor;
 
 use Illuminate\Http\UploadedFile;
-use Intervention\Image\Constraint;
-use Intervention\Image\Facades\Image;
-use Intervention\Image\Image as InterventionImage;
-use Spatie\LaravelImageOptimizer\Facades\ImageOptimizer;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Interfaces\ImageInterface;
+use Spatie\ImageOptimizer\OptimizerChainFactory;
+
 
 class ImageStorage extends Storage
 {
@@ -27,9 +27,17 @@ class ImageStorage extends Storage
     {
         $attributes = $this->resize($file);
 
+        $file = new UploadedFile(
+            path: $attributes['path'],
+            originalName: $attributes['file'],
+            mimeType: $attributes['mime']
+        );
+
         $file->storePubliclyAs('', $attributes['file'], [
             'disk' => $this->disk
         ]);
+
+        unset($attributes['path']);
 
         return $attributes;
     }
@@ -41,41 +49,49 @@ class ImageStorage extends Storage
 
         $maxWidth = $config['max-width'];
         $maxHeight = $config['max-height'];
+        $quality = $config['max-quality'];
 
         $name = $this->fileName($file);
         $fileName = $this->makeTargetFilename($file, $name);
         $filePath = $this->makeTargetFilePath($fileName);
 
-        $image = $this->resizeImage($file, $maxWidth, $maxHeight)->save($filePath, $config['max-quality']);
+        $image = $this->resizeImage($file, $maxWidth, $maxHeight);
+        $this->save($image, $quality, $filePath);
 
         return [
             'file'   => $fileName,
+            'path'   => $filePath,
             //'name'   => '$hash',
             'disk'   => $this->disk,
-            'mime'   => $image->mime(),
+            'mime'   => $image->origin()->mimetype(),
             'width'  => $image->width(),
             'height' => $image->height(),
             'size'   => $this->optimize($filePath),
         ];
     }
 
-    protected function resizeImage(UploadedFile $file, int $maxWidth, int $maxHeight): InterventionImage
+    protected function resizeImage(UploadedFile $file, int $maxWidth, int $maxHeight): ImageInterface
     {
-        $image = Image::make($file->getRealPath());
+        $image = ImageManager::gd()->read($file->getRealPath());
 
-        if ($image->width() > $maxWidth || $image->height() > $maxHeight) {
-            $image->resize($maxWidth, $maxHeight, function(Constraint $constraint) {
-                $constraint->aspectRatio();
-                $constraint->upsize();
-            });
-        }
+        return $image->scaleDown($maxWidth, $maxHeight);
+    }
 
-        return $image;
+    protected function save(ImageInterface $image, int $quality, string $path): void
+    {
+        $image = match ($image->origin()->mimetype()) {
+            'image/webp' => $image->toWebp($quality),
+            'image/avif' => $image->toAvif($quality),
+            'image/jpeg' => $image->toJpeg($quality),
+            default      => $image->encode(),
+        };
+
+        $image->save($path);
     }
 
     public function optimize(string $tempPath): int
     {
-        ImageOptimizer::optimize($tempPath);
+        OptimizerChainFactory::create()->optimize($tempPath);
 
         return filesize($tempPath);
     }
